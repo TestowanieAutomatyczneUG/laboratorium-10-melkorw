51d8f7a1c00d805c249018bd034f7c95f4611c7d
Olek <aleksander.wardyn@jit.team>
2020-12-07 16:07:08 +0100

    Prepare classes for testing

---
+++ src/notes_service/notes_service.py
@@ -0,0 +1,19 @@
+from src.notes_storage.notes_storage import NotesStorage
+
+
+class NotesService:
+    def __init__(self):
+        self.notes_storage = NotesStorage()
+
+    def add(self, note):
+        self.notes_storage.add(note)
+
+    def average_of(self, name):
+        result = self.notes_storage.get_all_notes_of(name)
+        wynik = 0
+        for note in result:
+            wynik += note.get_note()
+        return wynik/len(result)
+
+    def clear(self):
+        self.notes_storage.clear()
---
+++ src/notes_storage/notes_storage.py
@@ -0,0 +1,12 @@
+class NotesStorage:
+    def __init__(self):
+        self.notes = []
+
+    def add(self, note):
+        pass
+
+    def clear(self):
+        pass
+
+    def get_all_notes_of(self, name):
+        pass
